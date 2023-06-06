c3cb092451b501cc46c66fa925d89accc7964a53
bajajshubham <shubham2bajaj@gmail.com>
2023-06-06 23:32:01 +0200

    added dummy movie choice route

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const cors = require("cors");
 const loginRoutes = require("./api/routes/login");
+const playground = require("./api/routes/playground");
 
 //===================CONFIGS=======================//
 const app = express();
@@ -15,5 +16,6 @@ app.get("/", (req, res, next) => {
 
 // app.use("/api/auth", authRoute);
 app.use("/api/login", loginRoutes);
+app.use("/play", playground);
 
 module.exports = app;
