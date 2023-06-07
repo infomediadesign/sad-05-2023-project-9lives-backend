5f1aba92c4c1b9a68afb21b5f58ad8ccb88ace4c
bajajshubham <shubham2bajaj@gmail.com>
2023-06-08 01:00:48 +0200

    successfully sending access token in response

--- api/routes/login.js
+++ api/routes/login.js
@@ -5,9 +5,32 @@ const jwt = require("jsonwebtoken");
 
 router.post("/", async (req, res, next) => {
   let isPassMatch = false;
-  const user = await UserLogin.findOne({ email: req.body.email }).exec();
-  isPassMatch = bcrypt.compareSync(req.body.password, user.password);
-  res.status(200).json({ m: isPassMatch });
+  try {
+    const user = await UserLogin.findOne({ email: req.body.email }).exec();
+    if (!user) {
+      res.status(401).json("User not found");
+    } else {
+      isPassMatch = bcrypt.compareSync(req.body.password, user.password);
+      if (!isPassMatch) {
+        res.status(401).json("Incorrect password");
+      } else {
+        const accessToken = jwt.sign(
+          {
+            id: user._id,
+            // isAdmin: user.isAdmin,
+          },
+          process.env.JWT_SEC,
+          { expiresIn: "3d" }
+        );
+        const { password, ...others } = user._doc;
+
+        res.status(200).json({ ...others, accessToken });
+      }
+    }
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json(error.message);
+  }
 });
 
 module.exports = router;
